c6e5d2a35241e023cc436c3b309472d7ab05ea4d
simaoarrais <simaoarrais@gmail.com>
2025-05-23 19:42:00 +0100

    change to support various types of parameters

--- run.sh
+++ run.sh
@@ -1,3 +1,31 @@
-mvn clean install
-# ./mvnw -pl rest spring-boot:run
-# ./mvnw -pl calculator spring-boot:run
+#!/bin/bash
+
+# Check if an argument is passed
+if [ $# -eq 0 ]; then
+  echo "No command provided. Usage: ./run.sh [clean|build|deploy]"
+  exit 1
+fi
+
+case "$1" in
+  clean)
+    ./mvnw clean install
+    # ./mvnw clean package -pl calculator
+    # ./mvnw clean package -pl rest
+    ;;
+
+  build)
+    ./mvnw clean install
+
+    echo "Starting REST module..."
+    ./mvnw -pl rest spring-boot:run > rest.log 2>&1 &
+
+    echo "Starting Calculator module..."
+    ./mvnw -pl calculator spring-boot:run > calculator.log 2>&1 &
+    ;;
+
+  *)
+    echo "Unknown command: $1"
+    echo "Usage: ./run.sh [clean|build|deploy]"
+    exit 1
+    ;;
+esac
