001c769411d0aca363692d205bb19a339bf4d508
simaoarrais <simaoarrais@gmail.com>
2025-05-27 02:26:22 +0100

    Add logger and remove print

--- rest/src/main/java/com/wit/config/RequestIdFilter.java
+++ rest/src/main/java/com/wit/config/RequestIdFilter.java
@@ -2,14 +2,21 @@ package com.wit.config;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+
+import com.wit.controller.CalculatorController;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Component
 public class RequestIdFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(CalculatorController.class);
 
     private static final String REQUEST_ID = "requestId";
 
@@ -21,7 +28,7 @@ public class RequestIdFilter implements Filter {
             MDC.put(REQUEST_ID, requestId);
 
             HttpServletRequest req = (HttpServletRequest) servletRequest;
-            System.out.println("Received " + req.getMethod() + " " + req.getRequestURI() + " [requestId=" + requestId + "]");
+            logger.debug("Received {} {} [requestId={}]", req.getMethod(), req.getRequestURI(), requestId);
 
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
